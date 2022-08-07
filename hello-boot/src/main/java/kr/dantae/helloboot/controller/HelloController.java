9a68fb6230d2110e6284dc9fff809353e2305267
A <A@DESKTOP-BMSVAHF>
2022-08-07 18:09:57 +0900

    hello-boot version 1.0

---
+++ hello-boot/src/main/java/kr/dantae/helloboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package kr.dantae.helloboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("hello")
+	public String hello() {
+		return "Hello Spring Boot";
+	}
+}
