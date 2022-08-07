2ffc0268f645c7c7f2cff356f5cf057256dd8a22
A <A@DESKTOP-BMSVAHF>
2022-08-07 22:29:52 +0900

    User Rest v 0.5

---
+++ users-rest/src/main/java/kr/dantae/rest/entity/User.java
@@ -0,0 +1,38 @@
+package kr.dantae.rest.entity;
+
+import java.util.UUID;
+
+import com.google.gson.Gson;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+	private String id;
+	private String name;
+	private String email;
+	
+	public User() {
+		id = UUID.randomUUID().toString();
+	}
+	
+	public User(String name, String email) {
+		this();
+		this.name = name;
+		this.email = email;
+	}
+	
+	public static User sample() {
+		return new User("dantae", "dantae@spring-boot.io");
+	}
+	
+	public static void main(String[] args) {
+		User user = User.sample();
+		System.out.println(new Gson().toJson(user));
+	}
+
+}
