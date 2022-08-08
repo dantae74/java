1b4b6a508ae4af4b3cc1516fdab8510de179c10b
dantae <49467366+dantae74@users.noreply.github.com>
2022-08-08 09:53:30 +0900

    Update UserStoreLogic.java

--- users-rest/src/main/java/kr/dantae/rest/store/logic/UserStoreLogic.java
+++ users-rest/src/main/java/kr/dantae/rest/store/logic/UserStoreLogic.java
@@ -28,26 +28,22 @@ public class UserStoreLogic implements UserStore {
 
 	@Override
 	public void update(User newUser) {
-		// TODO Auto-generated method stub
-
+		this.userMap.put(newUser.getId(), newUser);
 	}
 
 	@Override
 	public void delete(String id) {
-		// TODO Auto-generated method stub
-
+		this.userMap.remove(id);
 	}
 
 	@Override
 	public User retrieve(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.userMap.get(id);
 	}
 
 	@Override
 	public List<User> retrieveAll() {
-		// TODO Auto-generated method stub
-		return null;
+		this.userMap.values().stream().collect(Collectors.toList());
 	}
 
 }
